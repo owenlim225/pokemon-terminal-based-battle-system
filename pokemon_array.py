0611248182f6f7acb3dc9350f35d8a1a834fa987
Sherwin <87555304+owenlim225@users.noreply.github.com>
2024-09-30 10:51:22 +0800

    Update pokemon_array.py

--- pokemon_array.py
+++ pokemon_array.py
@@ -1,1 +1,24 @@
-pokemon
+#Limosnero, Sherwin P.
+# J2S
+# Machine Problem #1M
+
+
+
+
+
+
+class Mechanics:
+    def __init__(self) -> None:
+        pass
+    
+
+
+
+class Display:
+    def __init__(self) -> None:
+        pass
+    
+    
+
+if __name__ == "__main__":
+    pass
